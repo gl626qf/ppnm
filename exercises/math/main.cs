c1aa12e88aa35e7bcd2b0d14b8b51c3be75ca668
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-02-08 11:04:05 +0100

    1. part of math exercise

---
+++ exercises/math/main.cs
@@ -0,0 +1,13 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+class main{
+	static void Main(){
+		int n=2;
+		WriteLine(System.Math.Sqrt(2));
+		WriteLine(System.Math.Pow(2, 1.0/5.0));
+		WriteLine(Pow(E, PI));
+		WriteLine(Pow(PI, E));
+}
+}
