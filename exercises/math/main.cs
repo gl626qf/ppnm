e2641b16058dace4334a4039330ad913620a3127
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-02-08 14:15:41 +0100

    2. gamma func

--- exercises/math/main.cs
+++ exercises/math/main.cs
@@ -2,12 +2,19 @@ using System;
 using static System.Console;
 using static System.Math;
 
-class main{
-	static void Main(){
+public static class main{
+	public static void Main(){
 		int n=2;
 		WriteLine(System.Math.Sqrt(2));
 		WriteLine(System.Math.Pow(2, 1.0/5.0));
 		WriteLine(Pow(E, PI));
 		WriteLine(Pow(PI, E));
+		WriteLine(sfuns.gamma(1));
+		WriteLine(sfuns.gamma(2));
+		WriteLine(sfuns.gamma(3));
+		
+		WriteLine(sfuns.gamma(4));
+		WriteLine(sfuns.gamma(10));
+		
 }
 }
