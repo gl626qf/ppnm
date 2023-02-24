33678fe56fb8f6a206913e81b19602a836295005
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-02-24 15:57:14 +0100

    changes

--- exercises/vec/main.cs
+++ exercises/vec/main.cs
@@ -3,33 +3,66 @@ using static System.Console;
 using static System.Math;
 
 public static class main{
+	//Task 4
 	public static void print(this double x, string s){ /* x.print("x="); */
 		Write(s);WriteLine(x);
 	}
 	public static void print(this double x){ /*x.print() */
 		x.print("");
 	}
-	
+	// Task 7
+//	public override string ToString()
+//	{
+//		return $"{x} {y} {z}"; }
+
+
+	//Task 4 end
 
 	public static void Main(){
-
-		/*int n=2;
-		WriteLine(System.Math.Sqrt(2));
-		WriteLine(System.Math.Pow(2, 1.0/5.0));
-		WriteLine(Pow(E, PI));
-		WriteLine(Pow(PI, E));
-		WriteLine(sfuns.gamma(1));
-		WriteLine(sfuns.gamma(2));
-		WriteLine(sfuns.gamma(3));
-		
-		WriteLine(sfuns.gamma(4));
-		WriteLine(sfuns.gamma(10));
-		*/	
+		//Define vector examples and scalar
 		vec u = new vec(1,2,3);
 		vec v = new vec(2,4,6);
+		double c = 2;
+		WriteLine("I define the vectors");
 		u.print("u = ");
 		v.print("v = ");
-		vec vec_sum = u * v;
-		vec_sum.print("u * v =");
+
+		///Task 3
+		WriteLine("We define multiplication of a scalar");
+		vec vec_mult = c * v;
+		vec_mult.print("c * v =");
+		// This gives a bug: vec vec_mult_diff = v * c;		
+
+		WriteLine("We now define addition and subtraction");
+		vec vec_addition = u + v;
+		vec_addition.print("u + v = ");
+		vec vec_minus = -u;
+		vec_minus.print("-u = ");
+		vec vec_subtract = u - v;
+		vec_subtract.print("u - v = ");
+		///Task 3 end
+
+
+		///Task 5: Dot products
+		double vec_dot = u.dot(v);
+		vec_dot.print("scalar product u.dot(v) = ");
+		double vec_dot_new = vec.dot(u,v);
+		vec_dot_new.print("vec.dot(u,v) = ");
+		
+		// Syntactic sugar
+		WriteLine("We use some syntax sugar");
+		double vec_dot_syntax = u.dot_syntax(v);
+		vec_dot_syntax.print("u.dot_syntax(v) = ");
+		double vec_dot_new_syntax = vec.dot_syntax(u,v);
+		vec_dot_new_syntax.print("vec.dot_syntax(u,v) = ");
+
+		///Task 6: Approx methode
+		WriteLine("We make approximation function");
+		double approx_scalar = approx(2, 4);
+		//approx_scalar.print("approx_scalar test = ");
+				
+
+		///Task 7: ToString
+//		WriteLine(new v);
 	}
 }
--- exercises/vec/vec.cs
+++ exercises/vec/vec.cs
@@ -2,21 +2,45 @@ using System;
 using static System.Console;
 using static System.Math;
 public class vec{
+	//Task 1
 	public double x,y,z;
-	public vec (double a, double b, double c){this.x=a; this.y=b; this.z=c;}
 	public vec (){x=y=z=0; }
+	public vec (double a, double b, double c){this.x=a; this.y=b; this.z=c;}
 	
+	//Task 4
+	public void print(string s){Write(s); WriteLine($"{x} {y} {z}");}
+
+
 	public static vec operator*(vec v, double c){return new vec(c*v.x, c*v.y, c*v.z);}
 	
+	public static vec operator*(double c, vec v){return new vec(v.x*c, v.y*c, v.z*c);}	
+
 	
-	public void print(string s){Write(s); WriteLine($"{x} {y} {z}");}
-	/*
-	public static vec operator+(vec u, vec v){ /* u+v */
-	/*	return new vec(u.x+v.x, u.y+v.y, u.z+v.z);
-	/*
-	public static vec operator-(vec u, vec v){/* u-v */
-	/*	return new vec(u.x-v.x, u.y-v.y, u.z-v.z)
-	*/
-	//operators:
+	public static vec operator+(vec u, vec v){return new vec(u.x+v.x,u.y+v.y, u.z+v.z);}
+	
+	public static vec operator-(vec u){return new vec(-u.x, -u.y, -u.z);}
+
+	public static vec operator-(vec u, vec v){return new vec(u.x-v.x, u.y-v.y, u.z-v.z);}
+	
+
+	/// Task 5
+	public double dot(vec other) /* to be called as u.dot(v) */
+		{return this.x * other.x + this.y * other.y + this.z * other.z;}
+	
+	public static double dot(vec v, vec w) /* to be called as vec.dot(u,v) */
+		{return v.x*w.x + v.y*w.y + v.z*w.z;}
+	
+	public double dot_syntax(vec other) => this.x*other.x + this.y*other.y + this.z*other.z;
+
+	public static double dot_syntax(vec v, vec w) => v.x*w.x + v.y*w.y + v.z*w.z;
+
+
+	///Task 6
+	public static bool approx(double a, double b, double acc=1e-9, double eps=1e-9){
+		if(Abs(a-b)<acc) return true;
+		if(Abs(a-b)<(Abs(a) + Abs(b)) * eps) return true;
+		return false;
+	}
 }	
+
 
