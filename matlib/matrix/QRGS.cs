549f44ef9eef1f26a304ad2922871c7d33c601c1
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-03-08 10:22:43 +0100

    try

---
+++ matlib/matrix/QRGS.cs
@@ -0,0 +1,5 @@
+public static class QRGS{
+	   public static void decomp(matrix a, matrix r){ ... }
+	      public static vector solve(matrix Q, matrix R, vector b){ ... }
+	         public static double det(matrix R){ ... }
+}
