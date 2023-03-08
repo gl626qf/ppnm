d68751b9c652c4b9fd05ed7f27a96a9156e111c9
ThomasHM <42569930+gl626qf@users.noreply.github.com>
2023-03-08 10:59:26 +0100

    Update QRGS.cs

--- matlib/matrix/QRGS.cs
+++ matlib/matrix/QRGS.cs
@@ -1,6 +1,12 @@
 public static class QRGS{
 	   public static void decomp(matrix a, matrix r){
-	  
+		   matrix Q=A.copy(), R=new matrix(m,m);
+		for (int i =0; i<m; i++){
+			R[i,i]=Q[i].norm(); /* Q[i] points to the i−th columb */
+			Q[i]/=R[i,i];
+			for (int j=i+1; j<m; j++){
+				R[ i , j ]=Q[ i ].dot(Q[j]) ;
+				Q[j]−=Q[i]∗R[i,j]; } }
 	  
 	  
 	  
