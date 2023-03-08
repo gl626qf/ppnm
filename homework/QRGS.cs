242a42eebe49c6db77b0abf2246fb8d42cf3a7dd
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-03-08 10:23:33 +0100

    news

---
+++ homework/QRGS.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Console;
+using System.Math;
+
+
+public static void main{
+public static class QRGS{
+	   public static void decomp(matrix a, matrix r){ ... }
+	      public static vector solve(matrix Q, matrix R, vector b){ ... }
+	         public static double det(matrix R){ ... }
+}
+} // main
+
+
+
