b4fd2e20b41c0c3920d0f309f290ff2b0941d145
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-03-10 13:36:38 +0100

    new

--- exercises/math/main.cs
+++ exercises/math/main.cs
@@ -4,17 +4,24 @@ using static System.Math;
 
 public static class main{
 	public static void Main(){
-		int n=2;
-		WriteLine(System.Math.Sqrt(2));
-		WriteLine(System.Math.Pow(2, 1.0/5.0));
-		WriteLine(Pow(E, PI));
-		WriteLine(Pow(PI, E));
+		/*sqrt(2), 2**(1/5), e**pi, pi**e */
+		double sqrt = System.Math.Sqrt(2);
+		string sqrtMessage = $"square root of 2 is {sqrt}";
+		WriteLine(sqrtMessage);
+		double twoPower =  System.Math.Pow(2, 1.0/5.0);
+		WriteLine($"2 to the power of 1/5 is {twoPower}");
+		double ePower = Pow(E, PI);
+		WriteLine($"E**PI is {ePower}");
+		double piPower = Pow(PI, E);
+		WriteLine($"Pow(PI, E) is {piPower}");
+		/*Stirling's approximation for 1,2,3,10 */
+		WriteLine("Here we print gamma function for 1, 2, 3 and 10");
 		WriteLine(sfuns.gamma(1));
 		WriteLine(sfuns.gamma(2));
 		WriteLine(sfuns.gamma(3));
-		
-		WriteLine(sfuns.gamma(4));
 		WriteLine(sfuns.gamma(10));
+		/*Now we print the ln to the gamma function */
+		WriteLine("We print the lngamma function");
 		
 }
 }
--- exercises/math/sfuns.cs
+++ exercises/math/sfuns.cs
@@ -7,6 +7,6 @@ public static class sfuns{
 		if(x<0)return PI/Sin(PI*x)/gamma(1-x); // Euler's reflection formula
 		if(x<9)return gamma(x+1)/x; // Recurrence relation
 		double lngamma=x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
-		return (lngamma);
+		return Exp(lngamma);
 		}
 }
