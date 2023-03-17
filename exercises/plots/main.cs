a4395cd97ad754967144d1c00b891b73a9952c71
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-03-17 17:47:32 +0100

    Cannot plot them seperately, need to fix

--- exercises/plots/main.cs
+++ exercises/plots/main.cs
@@ -4,8 +4,8 @@ static void Main(){
 	for(double x=-5+1.0/128;x<=5;x+=1.0/64){
 		WriteLine($"{x} {sfuns.gamma(x)}");
 	}
-    // for(double x = -5+1.0/128; x<=5; x+=1.0/64){
-    //     WriteLine($"{x} {sfuns.lngamma(x)}");
-    // }
+    for(double x = -50+1.0/128; x<=5; x+=1.0/64){
+        WriteLine($"{x} {sfuns.lngamma(x)}");
+    }
 }
 }
