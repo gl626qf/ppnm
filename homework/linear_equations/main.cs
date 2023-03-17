220701e57b1df112eeca9beeaf9da8aa05925ac3
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-03-17 23:28:30 +0100

    The Makefile can not find the path to matrix.dll, remember to add vector.dll too

---
+++ homework/linear_equations/main.cs
@@ -0,0 +1,32 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+
+public static class main{
+
+    public static void Main(){
+
+            WriteLine("------------TASK A------------"); // Maybe make it not hardcoded
+
+
+            // var A1 = matrix.random_matrix(5,4);
+            // A1.print("A=");
+
+            // var rnd = new System.Random(1); /* or any other seed */
+            // double x = rnd.NextDouble();
+            // double y = rnd.NextDouble();
+            // double z = rnd.NextDouble();
+
+            // WriteLine($"x, y, z = {x}, {y}, {z}");
+
+
+            // Testing vectors
+            /// HAVDE IKKE DET RIGTIGE I MIN MAKEFILE
+            WriteLine(matrix.matrix(1,2));
+
+
+	
+
+    }
+}
