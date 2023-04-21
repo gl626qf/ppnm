50edf322770edcb0e8064c0c115458acf7ab73d2
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-04-21 15:59:45 +0200

    Adding least squares

--- homework/least_squares/main.cs
+++ homework/least_squares/main.cs
@@ -2,18 +2,24 @@ using System;
 using static System.Console;
 using static System.Math;
 
+
+
 public static class main{
+
+
+    
 
     public static void Main(){
 
             WriteLine("------------TASK A------------"); // Maybe make it not hardcoded
 
 
+            //Making make the Next to Nextdouble
+
             //random tall
             // var random = new Random(Guid.NewGuid().GetHashCode());
             var random = new Random();
             int random_n = random.Next(2,7);
-            // Here we might implement + 1, so it is not quadratic
             int random_m = random.Next(2,random_n);
             // Here the matrix library is used 
             matrix random_a = new matrix(random_n, random_m);
@@ -23,22 +29,19 @@ public static class main{
                 }
             }
             
-            matrix a = random_a.copy();
-            matrix r = new matrix(random_m, random_m);
+            matrix Q = random_a.copy();
+            matrix R = new matrix(random_m, random_m);
 
-            a.print("This is A before decomposition");
-            r.print("This is R before decomposition");
+            Q.print("This is Q before decomposition");
+            R.print("This is R before decomposition");
 
 
             //We use decomposition
-
-
-
-            // QRGS.decomp(a, r);
+            QRGS.decomp(Q, R);
             
+            //We check the different matrices after decomp
+            Q.print("This is Q after decomposition");
+            R.print("This is R after decomposition");
 
-
-
-
-    }
-}
+    } //Main
+} //main
