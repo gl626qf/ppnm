c4f94596c9b8f36a0f59b4a97f8ae3b378004ad4
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-05-16 09:52:07 +0200

    Starting splines

---
+++ homework/ODE/main.cs
@@ -0,0 +1,77 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+
+
+
+
+public static class binsearch{ // maybe change name
+    public static int binsearch(double[] x, double z)
+        {/* locates the interval for z by bisection */ 
+        if(!(x[0]<=z && z<=x[x.Length-1])) throw new Exception("binsearch: bad z");
+        int i=0, j=x.Length-1;
+        while(j-i>1){
+            int mid=(i+j)/2;
+            if(z>x[mid]) i=mid; else j=mid;
+            }
+        return i;
+        }
+}
+
+public static class linspline{
+    public static double linterp(double[] x, double[] y, double z){
+            int i=binsearch(x,z);
+            double dx=x[i+1]-x[i]; if(!(dx>0)) throw new Exception("uups...");
+            double dy=y[i+1]-y[i];
+            return y[i]+dy/dx*(z-x[i]);
+        }
+
+}
+
+
+
+
+
+public static class main{
+
+    public static void Main(){
+
+            WriteLine("------------TASK A------------"); // Maybe make it not hardcoded
+
+
+            //Making make the Next to Nextdouble
+
+            //random tall
+            // var random = new Random(Guid.NewGuid().GetHashCode());
+            var random = new Random();
+            int random_n = random.Next(2,7);
+            int random_m = random.Next(2,random_n);
+            // Here the matrix library is used 
+            matrix random_a = new matrix(random_n, random_m);
+            for(int i=0;i<random_n;i++){
+                for(int j=0;j<random_m;j++){
+                    random_a[i,j] = random.NextDouble();
+                }
+            }
+            
+            matrix Q = random_a.copy();
+            matrix R = new matrix(random_m, random_m);
+
+            Q.print("This is Q before decomposition");
+            R.print("This is R before decomposition");
+
+
+
+
+            
+
+
+
+
+            WriteLine("------------TASK B------------");
+        
+
+
+    }
+}
