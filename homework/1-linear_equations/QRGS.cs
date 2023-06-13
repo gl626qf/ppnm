7ce9175dfa82be5e69b86b99b95ce25fd9becfa5
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-06-13 09:40:45 +0200

    Testing added to assgn. 1

---
+++ homework/1-linear_equations/QRGS.cs
@@ -0,0 +1,64 @@
+using System;
+using static System.Console;
+using static System.Math;
+public static class QRGS{
+
+
+		public static void decomp(matrix a, matrix r){
+			int m = r.size1;
+			for(int i=0;i<m;i++){
+				double ai_norm = a[i].norm();
+				matrix.set(r,i,i,ai_norm);
+			      	a[i] = a[i]/r[i,i];
+				for(int j=i+1;j<m;j++){
+					r[i,j] = a[i].dot(a[j]);
+					a[j] = a[j] - a[i]*r[i,j];
+				}
+			}
+		}
+
+
+
+		public static vector solve(matrix Q, matrix R, vector b){
+			vector x = Q.T*b;
+			for(int i=x.size-1;i>=0;i--){
+				double sum=0;
+				for(int k=i+1;k<x.size;k++) sum = sum + R[i,k]*x[k];
+				x[i] = (x[i]-sum)/R[i,i];
+			}
+			return x;
+		}
+		
+		// public static matrix inverse(matrix A, matrix Q = null, matrix R = null){
+
+
+		// }
+
+		
+		// public static matrix inverse(matrix A, matrix Q = null, matrix R = null){
+		// 	//(int n, int m) = (A.size1, A.size2);
+		// 	//Debug.Assert(n==m,string.Format("Only square matrices are invertible. A has dimension ({0},{1})",n,m));
+		// 	//WriteLine($"n = {n}, m = {m}");
+
+		// 	int n = A.size1;
+		// 	matrix invA = new matrix(n,n);
+		// 	vector ei = new vector(n);
+		// 	ei[0] = 1;
+			
+		// 	if((Q == null) || (R == null)){
+		// 		(Q, R) = decomp(A);
+		// 	}
+		// 	vector xi = solve(Q,R,ei);
+		// 	invA[0] = xi;
+		// 	for(int i=1; i<n; i++){
+		// 		ei[i-1] = 0;
+		// 		ei[i] = 1;
+		// 		xi = solve(Q,R,ei);
+		// 		invA[i] = xi;
+		// 	}
+		// 	return invA;
+		// }
+
+
+	    // public static double det(matrix R){ ... }
+}
---
+++ homework/1-linear_equations/main.cs
@@ -0,0 +1,181 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+
+
+public class main{
+
+
+
+    
+    public static bool checkUpperTriangular(matrix A){
+        
+
+        int N = A.size1;
+        for (int i = 1; i < N; i++){
+            for (int j = 0; j < i; j++){
+                if (A[i, j] != 0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static bool checkQTQ(matrix A, matrix B){
+        return B.approx(A);
+
+    }
+
+
+
+
+
+    
+
+    public static void Main(){
+
+            WriteLine("------------TASK A------------"); // Maybe make it not hardcoded
+
+
+            //Making make the Next to Nextdouble
+
+            //random tall
+            // var random = new Random(Guid.NewGuid().GetHashCode());
+            var random = new Random();
+            int random_n = random.Next(2,7);
+            int random_m = random.Next(2,random_n);
+            // Here the matrix library is used 
+            matrix random_a = new matrix(random_n, random_m);
+            for(int i=0;i<random_n;i++){
+                for(int j=0;j<random_m;j++){
+                    random_a[i,j] = random.NextDouble();
+                }
+            }
+            
+
+
+
+            WriteLine("\n \n");
+            WriteLine("----Decomp Testing----");
+
+
+            matrix A = random_a.copy();
+            matrix Q = random_a.copy();
+            matrix R = new matrix(random_m, random_m);
+
+            Q.print("This is Q before decomposition");
+            R.print("This is R before decomposition");
+
+
+            //We use decomposition
+            QRGS.decomp(Q, R);
+            
+            //We check the different matrices after decomp
+            Q.print("This is Q after decomposition");
+            R.print("This is R after decomposition");
+
+
+            //We need to check that R is upper triangular
+            // WriteLine($"checkUpperTriangular(R)) is {checkUpperTriangular(R)}";
+
+
+            WriteLine($"checkUpperTriangular(R) is {checkUpperTriangular(R)}");
+        
+
+        	R.print("Check that R is upper triangular, R = \n");
+            WriteLine($"Testing if Q.T * A is equal to the identity matrix: {((Q.T)*Q).approx(matrix.id(random_m))}");
+
+
+            WriteLine($"QR = A: {A.approx(Q*R)}");
+
+
+
+            
+
+            WriteLine("\n \n");
+            WriteLine("----Solve Testing----");
+            // Making square matrix
+            matrix random_square = new matrix(random_n, random_n);
+            for(int i=0;i<random_n;i++){
+                for(int j=0;j<random_n;j++){
+                    random_square[i,j] = random.NextDouble();
+                }
+            }
+            random_square.print("Random square matrix");
+
+
+            vector random_b = new vector(random_n);
+
+            for (int i=0; i<random_n;i++){
+                random_b[i] = random.NextDouble();
+            }
+            random_b.print("Random vector of size b");
+
+
+            // Making square matrix
+            matrix A_square = random_square.copy();
+
+            matrix R_square = new matrix(random_n, random_n);
+            vector b_square = random_b.copy();
+
+            A_square.print("A before QR");
+            R_square.print("R before QR");
+            
+            QRGS.decomp(A_square,R_square);
+            
+            A_square.print("A after QR");
+            R_square.print("R after QR");
+
+
+            // WriteLine($"Testing if A.T * A is equal to the identity matrix: {(A_square.T * A_square).approx(matrix.id(random_n))}");
+
+
+            //Now we need to solve 
+
+            vector x = QRGS.solve(A_square, R_square, b_square);
+            x.print("Solution vector x");
+
+            //Checking Ax = b. Need to Convert the A matrix back by multiplying with R
+            vector Ax = (A_square * R_square) * x;
+
+
+            WriteLine("We see if Ax = b");
+            // Ax.print("Ax");
+            // b_square.print("b");
+            WriteLine($"Checking if they are equal : {Ax.approx(b_square)}");
+
+
+
+
+            WriteLine("------------TASK B------------");
+
+            // matrix C_square = random_square.copy();
+            
+
+
+            // matrix C = QRGS.inverse(C_square);
+
+            
+
+            // C_square.print("Random C_square matrix");
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+    }
+}
