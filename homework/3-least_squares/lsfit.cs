04c168b28f3de8ea63774f7ceebf74c559ed9a58
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-06-25 11:41:20 +0200

    Got the scaling of the cov matrix to work

--- homework/3-least_squares/lsfit.cs
+++ homework/3-least_squares/lsfit.cs
@@ -1,58 +1,41 @@
-// using System;
-// using static System.Console;
-
-
-// namespace linalg{
-// public static class LS{
-// 	public static (vector, matrix) lsfit(Func<double,double>[] fs, vector x, vector y, vector dy){
-// 		int n = x.size;
-// 		int m = fs.Length;
-// 		matrix A = new matrix(n,m);
-// 		vector b = new vector(n);
-
-// 		for(int i=0 ; i<n ; i++){
-// 			b[i] = y[i]/dy[i];
-// 			for(int j=0 ; j<m ; j++){
-// 				A[i,j] = fs[j](x[i])/dy[i];
-// 			}
-// 		}
-//         // matrix R = new matrix(n,m);
-//         // WriteLine(R);
-// 		(matrix Q, matrix R) = QRGS.decomp(A);
-//         WriteLine(Q);
-// 		vector c = QRGS.solve(Q,R,b);
-//         matrix S = Q.copy();
-// 		// matrix ATA = (A.T)*A;
-// 		// matrix S = QRGS.inverse(ATA);
-// 		return (c,S);
-// 	}
-// }
-// }
-
 using System;
 using static System.Math;
 using static System.Console;
 public static class lsfit{
-	public static (vector,matrix) LS(vector x, vector y, vector dy, Func<double,double>[] f){
+	public static (vector,matrix) LS(vector x, vector y, vector dy, Func<double,double>[] fs){
 		int n = x.size;
-		int m = f.Length;
-		//makes matrix A with Aik=fk(xi)/dyi and bi =yi/dyi
+		int m = fs.Length;
+	
+		// for(int i = 0;i<n;i++){
+		// 	for(int k = 0;k<m;k++){
+		// 	double d = dy[i];	
+		// 	A[i,k]=f[k](x[i])/d;
+		// 	}
+		// b[i] = y[i]/dy[i];
+		// }
+
+
 		matrix A = new matrix(n,m);
 		vector b = new vector(n);
-	
-		for(int i = 0;i<n;i++){
-			for(int k = 0;k<m;k++){
-			double d = dy[i];	
-			A[i,k]=f[k](x[i])/d;
+		for(int i=0 ; i<n ; i++){
+			b[i] = y[i]/dy[i];
+			for(int j=0 ; j<m ; j++){
+				A[i,j] = fs[j](x[i])/dy[i];
 			}
-		b[i] = y[i]/dy[i];
 		}
 
 		//decomposing A=QR with QRGS.cs
-		var (Q,R) = QRGS.decomp(A);
+		(matrix Q, matrix R) = QRGS.decomp(A);
 		//solvingi with QRGS.cs
 		vector c = QRGS.solve(Q,R,b);
-		matrix S = QRGS.inverse(Q); //finds (R^TR^)-1
+		matrix RTR = R.T * R;
+		R.print("R = ");
+		RTR.print("RTR = ");
+		// matrix S = QRGS.inverse(RTR, R); 
+		matrix S = QRGS.inverseATA(R);
+
+		S.print("S = ");
+
 	
 	return (c,S);	
         }
@@ -62,25 +45,3 @@ public static class lsfit{
 
 
 
-// using System;
-// public static partial class matlib{
-
-// public static (vector,matrix) lsfit(
-// 	Func<double,double>[] fs, vector x, vector y, vector dy){
-// 	int n = x.size, m=fs.Length;
-// 	var A = new matrix(n,m);
-// 	var b = new vector(n);
-// 	for(int i=0;i<n;i++){
-// 		b[i]=y[i]/dy[i];
-// 		for(int k=0;k<m;k++)A[i,k]=fs[k](x[i])/dy[i];
-// 		}
-// 	var qra = new GSQR(A);
-// 	vector c = qra.solve(b);
-// 	matrix invA = qra.pinverse();
-// 	matrix S = invA*invA.T;
-// //	var invR = (new GSQR(qra.R)).inverse();
-// //	var S = invR*invR.T;
-// 	return (c,S);
-// 	}//lsfit
-
-// }//class
--- homework/3-least_squares/main.cs
+++ homework/3-least_squares/main.cs
@@ -24,9 +24,7 @@ public class main{
 		// StreamWriter outPartB = new StreamWriter("OutPartB.txt", false);
 		// StreamWriter dataPartABC = new StreamWriter("DataPartABC.data",false);
 
-		// WriteLine("Measure of radioactivity of the element ThXat time, the data is defined and solving fit by QR decomposition. The result is the vector c:");	
-		// vector t = new vector(new double [] {1,2,3,4,6,9,10,13,15});
-		// vector y = new vector(new double [] {117,100,88,72,53,29.5,25.2,15.2,11.1});
+
 
 		// vector dy = new vector(new double [] {5,5,5,4,4,3,3,2,2});
 		// var fs = new Func<double,double>[] {z=> 1.0, z => z};
@@ -90,9 +88,9 @@ public class main{
 		
 		foreach(var arg in args){
 			if(arg == "-checkQR"){
-				var A = matrix.randomMatrix(8,3);
-				A.print("A = ");
-				(matrix Q, matrix R) = QRGS.decomp(A);
+				matrix A_ = matrix.randomMatrix(8,3);
+				A_.print("A = ");
+				(matrix Q, matrix R) = QRGS.decomp(A_);
 				Q.print("Q = ");
 				R.print("R = ");
 
@@ -102,10 +100,53 @@ public class main{
 				WriteLine($"Testing if Q.T * Q is equal to the identity matrix: {((Q.T)*Q).approx(matrix.id(Q.size2))}");
 
 				WriteLine($"Testing if R is upper triangular. Test returns: {checkUpperTriangular(R)}");
+
+				WriteLine("---Checking if inverse works---");
+				matrix A_square = matrix.randomMatrix(8,8);
+				matrix A_test = A_square.copy();
+				A_square.print("Random square matrix: A");
+
+				(matrix Q_square, matrix R_square) = QRGS.decomp(A_square);
+
+				matrix B = QRGS.inverse(Q_square, R_square);
+
+				B.print("The inverse matrix: B");
+
+
+				WriteLine($"Testing if A * B = I is: {((A_test) * B).approx(matrix.id(A_square.size1))}");
+
+
+
 				
 			}
 
-			if(arg == "-lsfit"){
+			if(arg == "-THX"){
+		
+				vector t = new vector(new double [] {1,2,3,4,6,9,10,13,15});
+				vector y = new vector(new double [] {117,100,88,72,53,29.5,25.2,15.2,11.1});
+
+				vector dy = new vector(new double [] {5,5,5,4,4,3,3,2,2});
+				var fs = new Func<double,double>[] {z=> 1.0, z => z};
+
+				vector lny = new vector(y.size);
+				vector dlny = new vector(dy.size);
+				for(int i = 0;i<y.size;i++){
+					lny[i] = Log(y[i]);
+					dlny[i] = dy[i]/y[i];
+				}
+				
+				(vector c, matrix S) = lsfit.LS(t, lny, dlny, fs); 		
+				c.print("c = ");
+				S.print("covariance matrix S = ");
+				double lna = c[0]; double lna_err = Sqrt(S[0,0]);
+				double a = Exp(lna); double a_err = Exp(lna_err);
+				double lambda = -c[1]; double lambda_err = Sqrt(S[1,1]);
+				WriteLine($"lambda = {lambda:f5} pm {lambda_err:f4}");
+				WriteLine($"T_0.5 = {Log(2)/lambda:f2} pm {(Log(2)/Pow(lambda,2)*lambda_err):f2} days");
+				WriteLine("Theoretical is found to be 3.8215 days from wikipedia https://en.wikipedia.org/wiki/Radon-222");
+
+
+
 
 			}
 		
