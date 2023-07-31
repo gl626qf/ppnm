fa49a0ab1abe98dbe5b4863246db8fdbe5fd49ad
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-07-31 15:27:28 +0200

    added swarm

--- exam/main.cs
+++ exam/main.cs
@@ -6,6 +6,29 @@ using static System.Math;
 public class main{
 
 
+    public static int CalculateFactorial(int n){
+        if (n == 0){return 1;}
+        return n * CalculateFactorial(n - 1);
+    }
+
+    public static double CalculateLogBinomialCoefficient(int n, int k)
+    {
+        double logNumerator = 0;
+        double logDenominator = 0;
+
+        for (int i = 1; i <= k; i++)
+        {
+            logNumerator += Math.Log(n - i + 1);
+            logDenominator += Math.Log(i);
+        }
+
+        return logNumerator - logDenominator;
+    }
+
+
+
+
+
     // Function to minimize (example)
     public static double function1(vector x)
     {
@@ -28,6 +51,47 @@ public class main{
 		double result = Log(x[0]) + 2 * Log(x[1]) + 0.5 * Log(x[2]);
 		return result;
 	}
+
+
+    // public static double gauss(vector x, double mu, double sigma){
+    //     // double K = 1 / (Sqrt(2*PI) * sigma) * Exp(-Pow(x-mu,2) / (2 * Pow(sigma,2)));
+    //     double K = 1 / (Sqrt(2*PI) * sigma);
+    //     double g_ = Exp(-Pow(x[0]-mu,2) / (2 * Pow(sigma,2)));
+    //     // WriteLine("Gaussian PDF");
+    //     // WriteLine($"Standard deviation, σ = {sigma}");
+    //     // WriteLine($"Average value, µ = {mu}");
+    //     return K * g_;
+
+    // } // gauss
+
+    public static double gauss(vector x){
+		double sigma = 1;
+		double mu = 0;
+        // double K = 1 / (Sqrt(2*PI) * sigma) * Exp(-Pow(x-mu,2) / (2 * Pow(sigma,2)));
+        double K = 1 / (Sqrt(2*PI) * sigma);
+        double g_ = Exp(-Pow(x[0]-mu,2) / (2 * Pow(sigma,2))) + x[1];
+        // WriteLine("Gaussian PDF");
+        // WriteLine($"Standard deviation, σ = {sigma}");
+        // WriteLine($"Average value, µ = {mu}");
+        return K * g_;
+
+    } // gauss
+
+    // public static double poisson(vector n, double v){
+    //     // double sigma = Sqrt(v);
+    //     // WriteLine($"Standard deviation, σ = {sigma}");
+    //     // WriteLine($"Average value, 𝛎 = {v}");
+    //     return Pow(v,n) / CalculateFactorial(n) * Exp(-v);
+    // }
+
+    // public static double binomial(vector r, int N, double p){
+    //     // double part1 = CalculateFactorial(N) / (CalculateFactorial(r) * CalculateFactorial(N - r));
+    //     // double part2 = Pow(p,r) * Pow((1- p), N-r);
+    //     // return part1 * part2;
+    //     double logPart1 = CalculateLogBinomialCoefficient(N, r);
+    //     double logPart2 = r * Math.Log(p) + (N - r) * Math.Log(1 - p);
+    //     return Math.Exp(logPart1 + logPart2);
+    // }   
 
 
     public static void Main(string[] args){
@@ -91,6 +155,23 @@ public class main{
 
 				solution.print("Solution: ");
 				Console.WriteLine("Function evaluated at vector: " + divFunction1(solution));
+			
+			}
+
+
+
+			if(arg == "-gauss"){
+				    
+				// // Initial vectors
+				vector a = new vector(1,2); 
+				vector b = new vector(2,3); 
+
+				// // Run algorithm 
+				vector solution = svarm.run(gauss, a, b, seconds: 2);
+
+				solution.print("Solution: ");
+				// vector solution = vector(2);
+				// Console.WriteLine("Function evaluated at vector: " + gauss(solution));
 			
 			}
 
